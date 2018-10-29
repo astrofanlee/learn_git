026db8567d9d6e14ab56ea12e9ec489fbcbf94d7
Li Jixia <astrofanlee@gmail.com>
2018-10-29 11:51:15 +0800

    add some labels
    add some labels x and y

--- a.py
+++ a.py
@@ -4,4 +4,6 @@ import matplotlib.pyplot as plt
 a = np.random.random(1000*1000)
 plt.pcolormesh(a)
 plt.figure(figsize=(12,9))
+plt.xlabel('x label')
+plt.ylabel('y label')
 plt.savefig('graph_a.png')
