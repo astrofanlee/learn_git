e0ea71306676217c9a6fcc7aba9f32521a4f1e7b
astrofanlee <astrofanlee@gmail.com>
2018-10-29 12:06:44 +0800

    run by python. bug fixed.

--- a.py
+++ a.py
@@ -1,9 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-a = np.random.random(1000*1000)
-plt.pcolormesh(a)
+a = np.random.random((1000, 1000))
 plt.figure(figsize=(12,9))
+plt.pcolormesh(a)
 plt.xlabel('x label')
 plt.ylabel('y label')
 plt.savefig('graph_a.png')
+plt.show()
+
